2d3acf84d7e95840bec3b9dd90e2486a3f0a8358
FaranakNehzati <faranak.nehzati@gmail.com>
2021-03-11 15:09:38 -0500

    second commit

---
+++ src/todosData.js
@@ -0,0 +1,29 @@
+const todosData = [
+    {
+        id: 1,
+        text: "Take out the trash",
+        completed: true
+    },
+    {
+        id: 2,
+        text: "Wash the dishes",
+        completed: false
+    },
+     {
+        id: 3,
+        text: "Feed the cats",
+        completed: true
+    },
+    {
+        id: 4,
+        text: "Go to the gym",
+        completed: false
+    },
+    {
+        id: 5,
+        text: "Do the laundry",
+        completed: false
+    }
+]
+
+export default todosData
